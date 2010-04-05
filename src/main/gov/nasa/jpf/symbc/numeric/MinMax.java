1295d8d6762a21b52179c99bd03058a7412e2046
Corina Pasareanu <corina.s.pasareanu@nasa.gov>
2010-04-05 11:56:44 -0700

    (no commit message)

--- src/main/gov/nasa/jpf/symbc/numeric/MinMax.java
+++ src/main/gov/nasa/jpf/symbc/numeric/MinMax.java
@@ -20,8 +20,8 @@
 package gov.nasa.jpf.symbc.numeric;
 
 public class MinMax {
-	public static int MININT = -1000000;//Integer.MIN_VALUE;//-10000;
-	public static int MAXINT = 1000000;// Integer.MAX_VALUE;//10000;
+	public static int MININT = -10000;//-1000000;//Integer.MIN_VALUE;//-10000;
+	public static int MAXINT = 10000;//1000000;// Integer.MAX_VALUE;//10000;
 	public static double MINDOUBLE = -10000.0;//-1.0e8;//Double.MIN_VALUE;
 	public static double MAXDOUBLE = 10000.0;//1.0e8; //Double.MAX_VALUE;
 }
