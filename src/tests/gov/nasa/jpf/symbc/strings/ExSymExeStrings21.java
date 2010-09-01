c6665bc8c998993b49f26a05aa49b572e17a3cd8
Gideon Redelinghuys <gjredelinghuys@gmail.com>
2010-09-01 19:09:54 +0200

    Moved string tests to their own subpackage, restored ExSymExe.jpf

---
+++ src/tests/gov/nasa/jpf/symbc/strings/ExSymExeStrings21.java
@@ -0,0 +1,30 @@
+package gov.nasa.jpf.symbc.strings;
+
+import gov.nasa.jpf.symbc.Debug;
+
+
+public class ExSymExeStrings21 {
+	static int field;
+
+  public static void main (String[] args) {
+	  String a="aaa";
+	  String b = "bbb";
+	  String c = "ccc";
+	  test (a,b, c);
+	  Debug.printPC("This is the PC at the end:");
+	  //a=a.concat(b);
+	  
+  }
+  
+  public static void test (String a, String b, String c) {
+	  if (a.equals(b)) {
+		  System.out.println("aaa");
+	  }
+	  if (b.equals(c)) {
+		  System.out.println("bbb");
+	  }
+
+  }
+
+}
+
