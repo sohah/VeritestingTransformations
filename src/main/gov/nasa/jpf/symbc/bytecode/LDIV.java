4fc458dfb9c37a475a213b4e6242df0ef10e8d3a
Corina Pasareanu <corina.s.pasareanu@nasa.gov>
2010-10-28 14:40:24 -0700

    (no commit message)

--- src/main/gov/nasa/jpf/symbc/bytecode/LDIV.java
+++ src/main/gov/nasa/jpf/symbc/bytecode/LDIV.java
@@ -2,12 +2,12 @@
 // Copyright (C) 2006 United States Government as represented by the
 // Administrator of the National Aeronautics and Space Administration
 // (NASA).  All Rights Reserved.
-// 
+//
 // This software is distributed under the NASA Open Source Agreement
 // (NOSA), version 1.3.  The NOSA has been approved by the Open Source
 // Initiative.  See the file NOSA-1.3-JPF at the top of the distribution
 // directory tree for the complete NOSA document.
-// 
+//
 // THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
 // KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
 // LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
@@ -34,14 +34,32 @@ public class LDIV extends gov.nasa.jpf.jvm.bytecode.LDIV  {
   @Override
   public Instruction execute (SystemState ss, KernelState ks, ThreadInfo th) {
 	    StackFrame sf = th.getTopFrame();
-	  
+
 		IntegerExpression sym_v1 = (IntegerExpression) sf.getOperandAttr(1);
 		IntegerExpression sym_v2 = (IntegerExpression) sf.getOperandAttr(3);
-	    
+
 	    if(sym_v1==null && sym_v2==null)
 	        return super.execute(ss, ks, th);// we'll still do the concrete execution
 	    else {
-	    	throw new RuntimeException("## Error: SYMBOLIC LDIV not supported");
+	    	long v1 = th.longPop();
+	    	long v2 = th.longPop();
+	    	th.longPush(0); // for symbolic expressions, the concrete value does not matter
+
+	    	IntegerExpression result = null;
+	    	if(sym_v1!=null) {
+	    		if (sym_v2!=null)
+	    			result = sym_v1._div(sym_v2);
+	    		else // v2 is concrete
+	    			result = sym_v1._div(v2);
+	    	}
+	    	else if (sym_v2!=null)
+	    		result = sym_v2._div_reverse(v1);
+
+	    	sf.setLongOperandAttr(result);
+
+	    	//System.out.println("Execute LDIV: "+result);
+
+	    	return getNext(th);
 	    }
   }
 
