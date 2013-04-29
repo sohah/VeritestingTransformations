b2b2c02ac17593ad41019ea4cff5c3b9b013af8c
Corina Pasareanu <corina.s.pasareanu@nasa.gov>
2013-04-29 16:47:42 -0700

    added fix for symbolic execution of TABLESWITCH

--- src/main/gov/nasa/jpf/symbc/bytecode/TABLESWITCH.java
+++ src/main/gov/nasa/jpf/symbc/bytecode/TABLESWITCH.java
@@ -19,18 +19,23 @@
 package gov.nasa.jpf.symbc.bytecode;
 
 import gov.nasa.jpf.JPFException;
+import gov.nasa.jpf.jvm.ChoiceGenerator;
 import gov.nasa.jpf.jvm.KernelState;
+import gov.nasa.jpf.jvm.StackFrame;
 import gov.nasa.jpf.jvm.SystemState;
 import gov.nasa.jpf.jvm.ThreadInfo;
 import gov.nasa.jpf.jvm.bytecode.Instruction;
-import gov.nasa.jpf.jvm.bytecode.InstructionVisitor;
+import gov.nasa.jpf.symbc.numeric.Comparator;
+import gov.nasa.jpf.symbc.numeric.IntegerExpression;
+import gov.nasa.jpf.symbc.numeric.PCChoiceGenerator;
+import gov.nasa.jpf.symbc.numeric.PathCondition;
 
 
 /**
  * Access jump table by index and jump
  *   ..., index  ...
  */
-public class TABLESWITCH extends SwitchInstruction implements gov.nasa.jpf.jvm.TableSwitchInstruction{
+public class TABLESWITCH extends gov.nasa.jpf.jvm.bytecode.SwitchInstruction implements gov.nasa.jpf.jvm.TableSwitchInstruction{
 
 	int min, max;
 
@@ -40,45 +45,118 @@ public class TABLESWITCH extends SwitchInstruction implements gov.nasa.jpf.jvm.T
 	    this.max = max;
 	  }
 
-	  public void setTarget (int value, int target){
-	    int i = value-min;
+	 
+	  @Override
+	  public Instruction execute (SystemState ss, KernelState ks, ThreadInfo ti) {  
+		StackFrame sf = ti.getTopFrame();
+		IntegerExpression sym_v = (IntegerExpression) sf.getOperandAttr();
+			
+		System.out.println("sym v "+ sym_v); 
+		if(sym_v==null) return super.execute(ss,ks,ti);
+		
+		// the condition is symbolic
+		ChoiceGenerator<?> cg;
+
+		if (!ti.isFirstStepInsn()) { // first time around
+			cg = new PCChoiceGenerator(targets.length+1);
+			((PCChoiceGenerator)cg).setOffset(this.position);
+			((PCChoiceGenerator)cg).setMethodName(this.getMethodInfo().getCompleteName());
+			ss.setNextChoiceGenerator(cg);
+			return this;
+		} else {  // this is what really returns results
+			cg = ss.getChoiceGenerator();
+			assert (cg instanceof PCChoiceGenerator) : "expected PCChoiceGenerator, got: " + cg;
+		}
+		sym_v = (IntegerExpression) sf.getOperandAttr();
+		ti.pop();
+		PathCondition pc;
+		//pc is updated with the pc stored in the choice generator above
+		//get the path condition from the
+		//previous choice generator of the same type
+
+		//TODO: could be optimized to not do this for each choice
+		ChoiceGenerator<?> prev_cg = cg.getPreviousChoiceGeneratorOfType(PCChoiceGenerator.class); 
+	
+		if (prev_cg == null)
+			pc = new PathCondition();
+		else
+			pc = ((PCChoiceGenerator)prev_cg).getCurrentPC();
+
+		assert pc != null;
+		//System.out.println("Execute Switch: PC"+pc);
+		int idx = (Integer)cg.getNextChoice();
+		//System.out.println("Execute Switch: "+ idx);
+
+		
+		if (idx == targets.length){ // default branch
+			lastIdx = -1;
+		
+			for(int i = 0; i< targets.length; i++)
+				pc._addDet(Comparator.NE, sym_v._minus(min), i);
+			// this could be replaced safely with only one constraint:
+			// pc._addDet(Comparator.GT, sym_v._minus(min), targets.length);
+			
+			if(!pc.simplify())  {// not satisfiable
+				ss.setIgnored(true);
+			} else {
+				//pc.solve();
+				((PCChoiceGenerator) cg).setCurrentPC(pc);
+				//System.out.println(((PCChoiceGenerator) cg).getCurrentPC());
+			}
+			return ti.getMethod().getInstructionAt(target);
+		} else {
+			lastIdx = idx;
+			pc._addDet(Comparator.EQ, sym_v._minus(min), idx);
+			if(!pc.simplify())  {// not satisfiable
+				ss.setIgnored(true);
+			} else {
+				//pc.solve();
+				((PCChoiceGenerator) cg).setCurrentPC(pc);
+				//System.out.println(((PCChoiceGenerator) cg).getCurrentPC());
+			}
+			return ti.getMethod().getInstructionAt(targets[idx]);
+		}
+
+		
+	  }
+
+	  protected Instruction executeConditional (SystemState ss, KernelState ks, ThreadInfo ti){
+			assert(false); // should not be called;
+		    int value = ti.pop();
+		    int i = value-min;
+		    int pc;
+
+		    if (i>=0 && i<targets.length){
+		      lastIdx = i;
+		      pc = targets[i];
+		    } else {
+		      lastIdx = -1;
+		      pc = target;
+		    }
+
+		    // <2do> this is BAD - we should compute the target insns just once
+		    return mi.getInstructionAt(pc);
+		  }
+	  
+	@Override
+	public void setTarget(int value, int target) {
+
+		int i = value-min;
 
 	    if (i>=0 && i<targets.length){
 	      targets[i] = target;
 	    } else {
 	      throw new JPFException("illegal tableswitch target: " + value);
 	    }
-	  }
-
-	  protected Instruction executeConditional (SystemState ss, KernelState ks, ThreadInfo ti){
-	    int value = ti.pop();
-	    int i = value-min;
-	    int pc;
-
-	    if (i>=0 && i<targets.length){
-	      lastIdx = i;
-	      pc = targets[i];
-	    } else {
-	      lastIdx = -1;
-	      pc = target;
-	    }
-
-	    // <2do> this is BAD - we should compute the target insns just once
-	    return mi.getInstructionAt(pc);
-	  }
+	}
 
 
-	  public int getLength() {
-	    return 13 + 2*(matches.length); // <2do> NOT RIGHT: padding!!
-	  }
-
-	  public int getByteCode () {
+	@Override
+	public int getByteCode() {
+		// TODO Auto-generated method stub
 	    return 0xAA;
-	  }
-
-	  public void accept(InstructionVisitor insVisitor) {
-		  insVisitor.visit(this);
-	  }
+	}
+	  
 
 
 }
