6275ac2a3d09b3316e93f16e399f0a5b21649822
sohah <husseinsoh@gmail.com>
2018-07-20 00:29:02 -0500

    changing phi stack propagation to be more structured

--- src/main/gov/nasa/jpf/symbc/veritesting/ast/transformations/substitution/StackSlotTable.java
+++ src/main/gov/nasa/jpf/symbc/veritesting/ast/transformations/substitution/StackSlotTable.java
@@ -42,60 +42,56 @@ public class StackSlotTable {
         return null;
     }
 
-    public void stackSlotPhiPropagation() {
-        boolean localVarUpdated;
+    private void stackSlotPhiPropagation() {
+        boolean changeDetected;
         do {
-            localVarUpdated = false;
-            Iterator<? extends SSAInstruction> phiIterator = ir.iteratePhis();
-            while (phiIterator.hasNext()) {
-                SSAPhiInstruction phiInstruction = (SSAPhiInstruction) phiIterator.next();
-                for (int use = 0; use < phiInstruction.getNumberOfUses(); use++) {
-                    int valNum = phiInstruction.getUse(use);
-                    if (!isConstant(valNum) && stackSlotMap.containsKey(valNum)) {
-                        if (updateLocalVarsForPhi(phiInstruction, valNum)) localVarUpdated = true;
-                        break;
+            changeDetected = false;
+            Iterator<? extends SSAInstruction> phiItr = ir.iteratePhis();
+            while (phiItr.hasNext()) {
+                SSAPhiInstruction phi = (SSAPhiInstruction) phiItr.next();
+                int result = phi.getDef();
+                int[] phiSlot = lookup(result);
+                if (phiSlot == null) {
+                    int[] slots = getOperandSlot(phi);
+                    if (slots != null) {
+                        stackSlotMap.put(phi.getDef(), slots);
+                        changeDetected = true;
                     }
+                } else {
+                    changeDetected = updatePhiUse(phi, phiSlot);
                 }
-                if (localVarUpdated) break;
-                for (int def = 0; def < phiInstruction.getNumberOfDefs(); def++) {
-                    int valNum = phiInstruction.getDef(def);
-                    if (!isConstant(valNum) && stackSlotMap.containsKey(valNum)) {
-                        if (updateLocalVarsForPhi(phiInstruction, valNum)) localVarUpdated = true;
-                        break;
-                    }
-                }
-                if (localVarUpdated) break;
             }
-        } while (localVarUpdated);
+        } while (changeDetected);
     }
 
+    private boolean updatePhiUse(SSAPhiInstruction phi, int[] slots) {
+        boolean changeDetected = false;
+        for (int i = 0; i < phi.getNumberOfUses(); i++) {
+            if (!isConstant(phi.getUse(i))) {
+                int[] useSckSlot = lookup(phi.getUse(i));
+                if (useSckSlot == null) {
+                    stackSlotMap.put(phi.getUse(i), slots);
+                    changeDetected = true;
+                }
+            }
+        }
+        return changeDetected;
+    }
+
+    private int[] getOperandSlot(SSAPhiInstruction phi) {
+        for (int i = 0; i < phi.getNumberOfUses(); i++) {
+            int[] useSckSlot = lookup(phi.getUse(i));
+            if (useSckSlot != null)
+                return useSckSlot;
+        }
+        return null;
+    }
 
     public boolean isConstant(int operand1) {
         SymbolTable table = ir.getSymbolTable();
         return table.isNumberConstant(operand1) ||
                 table.isBooleanOrZeroOneConstant(operand1) ||
                 table.isNullConstant(operand1);
-    }
-
-    private boolean updateLocalVarsForPhi(SSAPhiInstruction phiInstruction, int val) {
-        boolean ret = false;
-        for (int use = 0; use < phiInstruction.getNumberOfUses(); use++) {
-            int useValNum = phiInstruction.getUse(use);
-            if (useValNum == val || isConstant(useValNum)) continue;
-            if (!stackSlotMap.containsKey(useValNum)) {
-                stackSlotMap.put(useValNum, stackSlotMap.get(val));
-                ret = true;
-            }
-        }
-        for (int def = 0; def < phiInstruction.getNumberOfDefs(); def++) {
-            int defValNum = phiInstruction.getDef(def);
-            if (defValNum == val || isConstant(defValNum)) continue;
-            if (!stackSlotMap.containsKey(defValNum)) {
-                stackSlotMap.put(defValNum, stackSlotMap.get(val));
-                ret = true;
-            }
-        }
-        return ret;
     }
 
     public void printStackSlotMap() {
