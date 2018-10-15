244be981422f50efe70b32c53549ac8e097a422c
Vaibhav Sharma <vaibhav@umn.edu>
2018-10-14 20:55:01 -0500

    Adding a RunWBS class so that we can run WBS for a variable number of steps

---
+++ src/examples/RunWBS.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2014, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All rights reserved.
+ *
+ * Symbolic Pathfinder (jpf-symbc) is licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import gov.nasa.jpf.symbc.Debug;
+
+public class RunWBS {
+
+    public static void symbolicMethod() {}
+
+    public static void main(String[] args) {
+        WBS wbs = new WBS();
+        int maxSteps = 3;
+        for (int i = 0; i < maxSteps; i++) {
+            int pedal = 0; //Debug.makeSymbolicInteger("pedal" + i);
+            boolean auto = false; //Debug.makeSymbolicBoolean("auto" + i);
+            boolean skid = false; //Debug.makeSymbolicBoolean("skid" + i);
+            wbs.update(pedal, auto, skid);
+        }
+    }
+}
