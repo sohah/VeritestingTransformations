4960ba1570371b433da3a7625d303a89e017a70e
sohah <husseinsoh@gmail.com>
2018-12-21 15:27:11 -0600

    adding pad example modified from arbitrary state

--- src/examples/UnbeliveablySimplePad.java
+++ src/examples/UnbeliveablySimplePad.java
@@ -1,6 +1,9 @@
+import gov.nasa.jpf.symbc.Debug;
+
 public class UnbeliveablySimplePad {
     private boolean startBtn;
     private boolean launchBtn;
+    private boolean ignition;
 
     /*enum PadState {
         IDLE,
@@ -14,7 +17,9 @@ public class UnbeliveablySimplePad {
     final int IDLE = 1;
     final int READY = 2;
     final int LAUNCH = 3;
+    final int IGNITION = 4;
     final int INVALIDSTATE = 5;
+
 
 
     public UnbeliveablySimplePad() {
@@ -27,71 +32,108 @@ public class UnbeliveablySimplePad {
         int n = 1;
         boolean startBtn = false;
         boolean launchBtn = false;
-        pad.runPadSteps(n, startBtn, launchBtn);
+        boolean ignition = false;
+        boolean symVar = false;
+
+        pad.runPadSteps(n, startBtn, launchBtn, ignition, symVar);
+
     }
 
-    public void runPadSteps(int n, boolean startBtn, boolean launchBtn) {
-        boolean ignition;
+    public void runPadSteps(int n, boolean startBtn, boolean launchBtn, boolean ignition, boolean symVar) {
+
+        //make pad state symbolic.
         this.startBtn = startBtn;
         this.launchBtn = launchBtn;
-        ignition = runPad(n);
-        if (ignition)
-            launchRocket();
+        this.ignition = ignition;
+
+        boolean rockedLaunched = false;
+
+        if (n < 40) {
+            for (int i = 0; i < n; i++) {
+                if (symVar) {
+                    rockedLaunched = runPad(i); //running it here one step, but should be enclosed in a loop in real program.
+
+                    assert (rockedLaunched ? getCurrentState()==IGNITION : true);
+
+                    if (rockedLaunched) {
+                        resetPad();
+                        System.out.println("Rocket launched successfully.");
+                    } else
+                        System.out.println("Rocket still not launched.");
+                }
+            }
+        }
     }
 
-    private void launchRocket() {
-        System.out.println("Rocket launched successfully.");
+    private void resetPad() {
         startBtn = false;
         launchBtn = false;
+        ignition = false;
     }
 
     /**
      * It takes the input signal n, which either can be start, launch or empty, it has no reset. So basically, it must go fom "IDLE" to
      * "READY" to "LAUNCH", but it might stay indefinitely in any of the first two states. While it can only stay in the "LAUNCH" state for only one time slot.
+     *
      * @param n
      * @return
      */
     public boolean runPad(int n) {
-
-        // getting rid of definitions so that we can summarize the whole function
-//        boolean startSignal;
-//        boolean launchSignal;
-//        boolean emptySignal;
-
-        //startSignal = (n == 1);
-        //launchSignal = (n == 2);
-        //emptySignal = (!startSignal && !launchSignal);
-
         int currentState = getCurrentState();
 
-        boolean ignitionSignal = false;
-        if ((n == 1) || (n == 2)) { //only proceed if a non-empty signal was received.
-            if (currentState == IDLE) { // this condition is unbounded by time, so it is not part of the switch statement below.
-                if ((n == 1)) {
-                    startBtn = true;
-                }
-            } else { // !(currentState == PadState.IDLE)  && (!emptySignal)
-                if (currentState == READY) {
-                    if ((n == 2)) {
-                        launchBtn = true;
-                        ignitionSignal = true;
+        if (currentState == LAUNCH) {
+            launchBtn = false; //adding this to allow direct mapping to the jkind model that defines the iginition to be on in the following step where launchBtn was on.
+            ignition = true;
+        } else {
+            boolean startSignal;
+            boolean launchSignal;
+            boolean emptySignal;
+            boolean startOrLaunch;
+
+            startSignal = (n == 1);
+            launchSignal = (n == 2);
+            startOrLaunch = startSignal || launchSignal; //we have a problem in summarizing complex conditions
+
+            //emptySignal = (!startSignal && !launchSignal);
+
+            if(currentState == INVALIDSTATE){
+                resetPad();
+            }
+
+            if (startOrLaunch) { //only proceed if a non-empty signal was received, otherwise remain in the same state.
+                if (currentState == IDLE) { // this condition is unbounded by time, so it is not part of the switch statement below.
+                    if (startSignal) {
+                        startBtn = true;
+                    }
+                } else { // !(currentState == PadState.IDLE)  && (!emptySignal)
+                    if (currentState == READY) {
+                        if (launchSignal) {
+                            launchBtn = true;
+                        }
                     }
                 }
             }
         }
-        return ignitionSignal;
+        return ignition;
     }
 
     public int getCurrentState() {
         int padState;
-        if (!startBtn && !launchBtn) //IDLE State
+        boolean mystartBtn = this.startBtn;
+        boolean myLaunchBtn = this.launchBtn;
+        boolean myIgnition = this.ignition;
+
+        if (!mystartBtn && !myLaunchBtn && !myIgnition) //IDLE State
             padState = IDLE;
-        else if (startBtn && !launchBtn) //Ready State
+        else if (mystartBtn && !myLaunchBtn && !myIgnition) //Ready State
             padState = READY;
-        else if (!startBtn && launchBtn) // Launch State
+        else if (!mystartBtn && myLaunchBtn && !myIgnition) // Launch State
             padState = LAUNCH;
+        else if(!mystartBtn && !myLaunchBtn && myIgnition)
+            padState = IGNITION;
         else
             padState = INVALIDSTATE; // Invalid State
         return padState;
     }
+
 }
