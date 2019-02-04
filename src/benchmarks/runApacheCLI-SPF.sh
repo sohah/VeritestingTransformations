f49acb295a9a9d16c80f3ad293b9809e598e14a8
Vaibhav Sharma <vaibhav@umn.edu>
2019-02-04 17:26:51 -0600

    Adding various JPF config files for ApacheCLI and PrintTokens

---
+++ src/benchmarks/runApacheCLI-SPF.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+alias runSPF-cli='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/VeritestingTransformations/lib TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/VeritestingTransformations/build/apachecli/ java -Djava.library.path=/export/scratch2/vaibhav/VeritestingTransformations/lib -Xmx1024m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar '
+
+shopt -s expand_aliases
+VERIDIR=/export/scratch2/vaibhav/VeritestingTransformations
+for i in {3..9}; do
+  for j in {1..1}; do
+    echo "running ApacheCLI.$((i))sym.mode$((j))";
+    runSPF-cli $VERIDIR/src/apachecli/ApacheCLI.$((i))sym.mode$((j)).jpf >& $VERIDIR/logs/ApacheCLI.$((i))sym.mode$((j)).log 
+  done;
+done
+
+for i in {3..7}; do
+  for j in {1..1}; do
+    echo "running ApacheCLI.$((i))_1sym.mode$((j))";
+    runSPF-cli $VERIDIR/src/apachecli/ApacheCLI.$((i))_1sym.mode$((j)).jpf >& $VERIDIR/logs/ApacheCLI.$((i))_1sym.mode$((j)).log 
+  done;
+done
+
