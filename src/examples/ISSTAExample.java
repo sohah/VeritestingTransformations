5d6ce2e6b6bcf320c9cbb7d889c1f5302480e006
Vaibhav Sharma <vaibhav@umn.edu>
2019-03-09 22:53:41 -0600

    Updating the "ISSTAExample" aka the motivating example used in the FSE submission

--- src/examples/ISSTAExample.java
+++ src/examples/ISSTAExample.java
@@ -16,34 +16,28 @@ public class ISSTAExample {
 
 
     public int paperExampleNum(int x1, int x2, int x3, int x4, int x5, int x6) {
-
-        ArrayList<Integer> numberList = new ArrayList<Integer>(Arrays.asList(x1, x2, x3, x4, x5, x6));
-
+        List<Integer> list = new ArrayList<>(Arrays.asList(x1, x2, x3, x4, x5, x6));
+//        ArrayList<Integer> list = new ArrayList<>(200);
         int wordCount = 0;
         boolean inWord;
-
-        if (numberList.size() > 0) {
-            int firstEement = numberList.get(0);
-
-            if (firstEement == 0)
+        if (list.size() > 0) {
+            int firstElement = list.get(0);
+            if (firstElement == 0)
                 inWord = false;
-            else
-                inWord = true;
-
-            for (int i = 0; i < numberList.size(); i++) {
+            else inWord = true;
+            for (int i = 0; i < list.size(); i++) {
                 if (inWord) {
-                    if (numberList.get(i) == 0) { //0 means there is a whitespace
+                    if (list.get(i) == 0) { //0 is a delimiter
                         ++wordCount;
                         inWord = false;
                     }
                 } else {
-                    if (numberList.get(i) != 0) { // non whitespace.
+                    if (list.get(i) != 0) {
                         inWord = true;
                     }
                 }
             }
         }
-        System.out.println("Number of words is:" + wordCount);
         return wordCount;
     }
 
