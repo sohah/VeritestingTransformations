a6cb43c9b1d578096e5c93b77607902b09fd9e8d
sohah <husseinsoh@gmail.com>
2019-04-26 23:24:08 -0500

    pushing from laptop

--- src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/InputOutput/DiscoveryUtil.java
+++ src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/InputOutput/DiscoveryUtil.java
@@ -1,21 +1,74 @@
 package gov.nasa.jpf.symbc.veritesting.RangerDiscovery.InputOutput;
 
+import jkind.lustre.BinaryOp;
 import jkind.lustre.NamedType;
+import jkind.lustre.UnaryExpr;
+import jkind.lustre.UnaryOp;
+import za.ac.sun.cs.green.expr.Operation;
 
 import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract.*;
 import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract.lusterStringType;
+import static jkind.lustre.UnaryOp.NEGATIVE;
+import static jkind.lustre.UnaryOp.NOT;
 
 public class DiscoveryUtil {
-    public static NamedType stringToLusterType(String typeName){
-        if(typeName.equals("int"))
+    public static NamedType stringToLusterType(String typeName) {
+        if (typeName.equals("int"))
             return lusterIntType;
-        else if(typeName.equals("float"))
+        else if (typeName.equals("float"))
             return lusterFloatType;
-        else if(typeName.equals("bool"))
+        else if (typeName.equals("bool"))
             return lusterBoolType;
-        else if(typeName.equals("string"))
+        else if (typeName.equals("string"))
             return lusterStringType;
-        else{
+        else {
+            System.out.println("unknown type!");
+            assert false;
+        }
+        return null;
+    }
+
+
+    public static UnaryOp toLustreUnaryOp(Operation.Operator operator) {
+        if (operator.toString().equals("-"))
+            return NEGATIVE;
+        else if (operator.toString().equals("!"))
+            return NOT;
+        else {
+            System.out.println("unknown type!");
+            assert false;
+        }
+        return null;
+    }
+
+    public static BinaryOp toLustreBinaryOp(Operation.Operator operator) {
+        if (operator == Operation.Operator.ADD)
+            return BinaryOp.PLUS;
+        else if (operator == Operation.Operator.SUB)
+            return BinaryOp.MINUS;
+        if (operator == Operation.Operator.MUL)
+            return BinaryOp.MULTIPLY;
+        else if (operator == Operation.Operator.DIV)
+            return BinaryOp.DIVIDE;
+        else if (operator == Operation.Operator.EQ)
+            return BinaryOp.EQUAL;
+        else if (operator == Operation.Operator.DIV)
+            return BinaryOp.INT_DIVIDE;
+        else if (operator == Operation.Operator.NE)
+            return BinaryOp.NOTEQUAL;
+        else if (operator == Operation.Operator.GT)
+            return BinaryOp.GREATER;
+        else if (operator== Operation.Operator.LT)
+            return BinaryOp.LESS;
+        else if (operator == Operation.Operator.GE)
+            return BinaryOp.GREATEREQUAL;
+        else if (operator == Operation.Operator.LE)
+            return BinaryOp.LESSEQUAL;
+        else if (operator == Operation.Operator.OR)
+            return BinaryOp.OR;
+        else if (operator == Operation.Operator.AND)
+            return BinaryOp.AND;
+        else {
             System.out.println("unknown type!");
             assert false;
         }
--- src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/LustreTranslation/EquationExprVisitor.java
+++ src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/LustreTranslation/EquationExprVisitor.java
@@ -1,74 +1,134 @@
 package gov.nasa.jpf.symbc.veritesting.RangerDiscovery.LustreTranslation;
 
 import gov.nasa.jpf.symbc.veritesting.ast.def.*;
+import gov.nasa.jpf.symbc.veritesting.ast.def.IfThenElseExpr;
+import gov.nasa.jpf.symbc.veritesting.ast.transformations.Environment.DynamicRegion;
 import gov.nasa.jpf.symbc.veritesting.ast.visitors.ExprVisitor;
+import gov.nasa.jpf.symbc.veritesting.ast.visitors.ExprVisitorAdapter;
+import jkind.lustre.*;
+import jkind.lustre.Ast;
 import za.ac.sun.cs.green.expr.*;
 
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract.lusterFloatType;
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract.lusterIntType;
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract.lusterStringType;
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.InputOutput.DiscoveryUtil.stringToLusterType;
 
-public class EquationExprVisitor implements ExprVisitor {
+
+public class EquationExprVisitor implements ExprVisitor<jkind.lustre.Ast> {
+
+    DynamicRegion dynRegion;
+    ExprVisitorAdapter<Ast> eva;
+
+    public EquationExprVisitor(DynamicRegion dynRegion) {
+        this.dynRegion = dynRegion;
+        eva = new ExprVisitorAdapter<Ast>(this);
+    }
 
     @Override
-    public Object visit(IntConstant expr) {
+    public Ast visit(IntConstant expr) {
+        return new IntExpr(expr.getValue());
+    }
+
+    @Override
+    public Ast visit(IntVariable expr) {
+        return new VarDecl(expr.toString(), lusterIntType);
+    }
+
+    @Override
+    public Ast visit(Operation operation) {
+        int operationArity = operation.getArity();
+        if(operationArity==1){
+            Ast lusterOperand = eva.accept(operation.getOperand(0));
+            assert(lusterOperand instanceof Expr);
+            lusterOperand = new UnaryOp(operation.getOperator().toString());
+            return new UnaryExpr(, (Expr)lusterOperand);
+        }
         return null;
     }
 
     @Override
-    public Object visit(IntVariable expr) {
+    public Ast visit(RealConstant expr) {
+        System.out.println("Reals are not yet supported");
+        assert false;
         return null;
     }
 
     @Override
-    public Object visit(Operation expr) {
+    public Ast visit(RealVariable expr) {
+        return new VarDecl(expr.toString(), lusterFloatType);
+
+    }
+
+    @Override
+    public Ast visit(StringConstantGreen expr) {//TODO
+        System.out.println("Strings are not yet supported");
+        assert false;
         return null;
     }
 
     @Override
-    public Object visit(RealConstant expr) {
+    public Ast visit(StringVariable expr) {
+        return new VarDecl(expr.toString(), lusterStringType);
+    }
+
+    @Override
+    public Ast visit(IfThenElseExpr expr) {
+        Ast lusterCondition = eva.accept(expr.condition);
+        assert (lusterCondition instanceof Expr);
+
+        Ast lusterThenExpr = eva.accept(expr.thenExpr);
+        assert (lusterThenExpr instanceof Expr);
+
+        Ast lusterElseExpr = eva.accept(expr.elseExpr);
+        assert (lusterElseExpr instanceof Expr);
+
+        jkind.lustre.IfThenElseExpr lustreIfThenElse = new jkind.lustre.IfThenElseExpr((Expr) lusterCondition, (Expr) lusterThenExpr, (Expr) lusterElseExpr);
+
+        return lustreIfThenElse;
+    }
+
+    @Override
+    public Ast visit(ArrayRefVarExpr expr) {//TODO
+        System.out.println("Arrays are not yet supported");
+        assert false;
         return null;
     }
 
     @Override
-    public Object visit(RealVariable expr) {
-        return null;
+    public Ast visit(WalaVarExpr expr) {
+        String type = (String) dynRegion.varTypeTable.lookupByName(expr.toString());
+        assert (type != null);
+        return new VarDecl(expr.toString(), stringToLusterType(type));
     }
 
     @Override
-    public Object visit(StringConstantGreen expr) {
-        return null;
+    public Ast visit(FieldRefVarExpr expr) {
+        String type = dynRegion.fieldRefTypeTable.lookup(expr);
+        assert (type != null);
+        return new VarDecl(expr.toString(), stringToLusterType(type));
     }
 
     @Override
-    public Object visit(StringVariable expr) {
-        return null;
+    public Ast visit(GammaVarExpr expr) {
+        Ast lusterCondition = eva.accept(expr.condition);
+        assert (lusterCondition instanceof Expr);
+
+        Ast lusterThenExpr = eva.accept(expr.thenExpr);
+        assert (lusterThenExpr instanceof Expr);
+
+        Ast lusterElseExpr = eva.accept(expr.elseExpr);
+        assert (lusterElseExpr instanceof Expr);
+
+        jkind.lustre.IfThenElseExpr lustreIfThenElse = new jkind.lustre.IfThenElseExpr((Expr) lusterCondition, (Expr) lusterThenExpr, (Expr) lusterElseExpr);
+
+        return lustreIfThenElse;
     }
 
     @Override
-    public Object visit(IfThenElseExpr expr) {
-        return null;
-    }
-
-    @Override
-    public Object visit(ArrayRefVarExpr expr) {
-        return null;
-    }
-
-    @Override
-    public Object visit(WalaVarExpr expr) {
-        return null;
-    }
-
-    @Override
-    public Object visit(FieldRefVarExpr expr) {
-        return null;
-    }
-
-    @Override
-    public Object visit(GammaVarExpr expr) {
-        return null;
-    }
-
-    @Override
-    public Object visit(AstVarExpr expr) {
+    public Ast visit(AstVarExpr expr) {//TODO
+        System.out.println("return vars are not yet supported");
+        assert false;
         return null;
     }
 }
