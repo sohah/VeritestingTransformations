aa959a45f0cfe8190e229bf3f984ec17c5bb9739
sohah <husseinsoh@gmail.com>
2019-11-18 12:17:21 -0600

    adding a file for the there exists rPrime query

--- src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/Queries/MinimalRepair/MinimalRepairDriver.java
+++ src/main/gov/nasa/jpf/symbc/veritesting/RangerDiscovery/Queries/MinimalRepair/MinimalRepairDriver.java
@@ -1,15 +1,22 @@
 package gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Queries.MinimalRepair;
 
+import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Config;
 import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Contract;
 import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.LustreExtension.NoExtLustreVisitor;
 import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Queries.ARepair.synthesis.ARepairSynthesis;
+import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Util.DiscoveryUtil;
 import jkind.lustre.Program;
+
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.DiscoverContract.contractMethodName;
+import static gov.nasa.jpf.symbc.veritesting.RangerDiscovery.Util.DiscoveryUtil.writeToFile;
 
 public class MinimalRepairDriver {
 
     private static Contract contract;
     private static Program repairedProgram;
     private static ARepairSynthesis lastSynthizedContract;
+
+    public static int minimalLoopCount = 0;
 
     /**
      * This method initiates the discovery of finding minimal repair enclosed in the repairedProgram. It starts by
@@ -30,7 +37,10 @@ public class MinimalRepairDriver {
         repairedProgram = NoExtLustreVisitor.execute(repairedProgram);
 
         MinimalRepairSynthesis rPrimeExistsQ = new MinimalRepairSynthesis(lastSynthizedContract, repairedProgram.getMainNode());
-        System.out.println(rPrimeExistsQ);
-        return null;
+
+        String fileName = contractMethodName + "_" + minimalLoopCount + "_" + "rPrimeExists.lus";
+        writeToFile(fileName, rPrimeExistsQ.toString());
+
+        return rPrimeExistsQ.getSynthesizedProgram();
     }
 }
