93637e12aab09f85e578246a3d2a87811a254ce3
sohah <husseinsoh@gmail.com>
2020-02-09 11:12:54 -0600

    cleaning up and adding tcas

--- src/examples/DiscoveryTCAS.java
+++ src/examples/DiscoveryTCAS.java
@@ -1,43 +1,45 @@
+package examples;
 
-public class tcas2 {
+import gov.nasa.jpf.symbc.veritesting.RangerDiscovery.LustreTranslation.ToLutre;
+
+public class DiscoveryTCAS {
+
+    //free input
+    public static int Cur_Vertical_Sep;
+    public static int Own_Tracked_Alt;
+    public static int Own_Tracked_Alt_Rate;
+    public static int Other_Tracked_Alt;
+    public static int Alt_Layer_Value;
+    public static int Up_Separation;
+    public static int Down_Separation;
+    public static int Other_RAC;
+    public static int Other_Capability;
+    public static int Climb_Inhibit;
+
+
+    //all state input
     public static int OLEV = 600;
     public static int MAXALTDIFF = 300;
     public static int MINSEP = 600;
     public static int NOZCROSS = 100;
-
-    public static int Cur_Vertical_Sep;
     public static boolean High_Confidence;
     public static boolean Two_of_Three_Reports_Valid;
-
-    public static int Own_Tracked_Alt;
-    public static int Own_Tracked_Alt_Rate;
-    public static int Other_Tracked_Alt;
-
-    public static int Alt_Layer_Value;
-
     static int Positive_RA_Alt_Thresh_0;
     static int Positive_RA_Alt_Thresh_1;
     static int Positive_RA_Alt_Thresh_2;
     static int Positive_RA_Alt_Thresh_3;
-
-    public static int Up_Separation;
-    public static int Down_Separation;
-
-    public static int Other_RAC;
-
     public static int NO_INTENT = 0;
     public static int DO_NOT_CLIMB = 1;
     public static int DO_NOT_DESCEND = 2;
-
-    public static int Other_Capability;
     public static int TCAS_TA = 1;
     public static int OTHER = 2;
-
-    public static int Climb_Inhibit;
-
     public static int UNRESOLVED = 0;
     public static int UPWARD_RA = 1;
     public static int DOWNWARD_RA = 2;
+
+
+    //created field for output
+    private static int result_alt_sep_test = -1;
 
     public static void initialize() {
         Positive_RA_Alt_Thresh_0 = 400;
@@ -47,16 +49,13 @@ public class tcas2 {
     }
 
     public static int ALIM() {
-        if (Alt_Layer_Value == 0){
+        if (Alt_Layer_Value == 0) {
             return Positive_RA_Alt_Thresh_0;
-        }
-        else if (Alt_Layer_Value == 1){
+        } else if (Alt_Layer_Value == 1) {
             return Positive_RA_Alt_Thresh_1;
-        }
-        else if (Alt_Layer_Value == 2){
+        } else if (Alt_Layer_Value == 2) {
             return Positive_RA_Alt_Thresh_2;
-        }
-        else{
+        } else {
             return Positive_RA_Alt_Thresh_3;
         }
     }
@@ -65,8 +64,7 @@ public class tcas2 {
         if (Climb_Inhibit > 0) {
             int ret = Up_Separation + NOZCROSS;
             return ret;
-        }
-        else{
+        } else {
             return Up_Separation;
         }
     }
@@ -81,28 +79,23 @@ public class tcas2 {
         }
         if (upward_preferred != 0) {
             int alim = ALIM();
-            if(!(Down_Separation >= alim)){
+            if (!(Down_Separation >= alim)) {
                 return true;
-            }
-            else{
+            } else {
                 return false;
             }
-        }
-        else {
-            if (!(Cur_Vertical_Sep >= MINSEP)){
+        } else {
+            if (!(Cur_Vertical_Sep >= MINSEP)) {
                 return false;
-            }
-            else{
+            } else {
                 int alim = ALIM();
-                if(!(Up_Separation >= alim)){
+                if (!(Up_Separation >= alim)) {
                     return false;
-                }
-                else{
+                } else {
                     boolean own_above_thread = Own_Above_Threat();
-                    if (!own_above_thread){
+                    if (!own_above_thread) {
                         return false;
-                    }
-                    else{
+                    } else {
                         return true;
                     }
                 }
@@ -116,38 +109,31 @@ public class tcas2 {
         int inhibit_biased_climb = Inhibit_Biased_Climb();
         if (inhibit_biased_climb > Down_Separation) {
             upward_preferred = 1;
-        }
-        else {
+        } else {
             upward_preferred = 0;
         }
         if (upward_preferred != 0) {
             int alim = ALIM();
             boolean own_below_threat = Own_Below_Threat();
             // reduction source
-            if (!(Cur_Vertical_Sep >= MINSEP)){
+            if (!(Cur_Vertical_Sep >= MINSEP)) {
                 return false;
-            }
-            else if (!(Down_Separation >= alim)){
+            } else if (!(Down_Separation >= alim)) {
                 return false;
-            }
-            else if (!own_below_threat){
+            } else if (!own_below_threat) {
                 return false;
-            }
-            else{
+            } else {
                 return true;
             }
-        }
-        else {
+        } else {
             int alim = ALIM();
             boolean own_above_threat = Own_Above_Threat();
             // reduction source
-            if(!(Up_Separation >= alim)){
+            if (!(Up_Separation >= alim)) {
                 return false;
-            }
-            else if(!own_above_threat){
+            } else if (!own_above_threat) {
                 return false;
-            }
-            else{
+            } else {
                 return true;
             }
         }
@@ -155,7 +141,7 @@ public class tcas2 {
 
     public static boolean Own_Below_Threat() {
         boolean ret = false;
-        if(Own_Tracked_Alt < Other_Tracked_Alt){
+        if (Own_Tracked_Alt < Other_Tracked_Alt) {
             ret = true;
         }
         return ret;
@@ -163,43 +149,40 @@ public class tcas2 {
 
     public static boolean Own_Above_Threat() {
         boolean ret = false;
-        if(Other_Tracked_Alt < Own_Tracked_Alt){
+        if (Other_Tracked_Alt < Own_Tracked_Alt) {
             ret = true;
         }
         return ret;
     }
 
-    public static int alt_assign(){
+    public static int alt_assign() {
         int alt_sep = UNRESOLVED;
         boolean need_upward_RA = false;
         boolean non_crossing_biased_climb = Non_Crossing_Biased_Climb();
-        if(non_crossing_biased_climb){
+        if (non_crossing_biased_climb) {
             boolean own_below_threat = Own_Below_Threat(); //return symbolic temp variable
-            if(own_below_threat){
+            if (own_below_threat) {
                 need_upward_RA = true; //is symbolic
             }
         }
         boolean need_downward_RA = false;
         boolean non_crossing_biased_descend = Non_Crossing_Biased_Descend();
-        if(non_crossing_biased_descend){
+        if (non_crossing_biased_descend) {
             boolean own_above_threat = Own_Above_Threat();
-            if(own_above_threat){
+            if (own_above_threat) {
                 need_downward_RA = true;
             }
         }
-        if (need_upward_RA){
-            if(need_downward_RA){
+        if (need_upward_RA) {
+            if (need_downward_RA) {
                 alt_sep = UNRESOLVED;
-            }
-            else{
+            } else {
                 alt_sep = UPWARD_RA;
             }
-        }
-        else{
-            if (need_downward_RA){
+        } else {
+            if (need_downward_RA) {
                 alt_sep = DOWNWARD_RA;
-            }
-            else{
+            } else {
                 alt_sep = UNRESOLVED;
             }
         }
@@ -218,29 +201,28 @@ public class tcas2 {
         boolean intent_not_known = false;
         int alt_sep = UNRESOLVED;
 
-        if(High_Confidence){
-            if(Own_Tracked_Alt_Rate <= OLEV){
-                if(Cur_Vertical_Sep > MAXALTDIFF){
+        if (High_Confidence) {
+            if (Own_Tracked_Alt_Rate <= OLEV) {
+                if (Cur_Vertical_Sep > MAXALTDIFF) {
                     enabled = true;
                 }
             }
         }
 
-        if(enabled){
-            if(Other_Capability == TCAS_TA){
+        if (enabled) {
+            if (Other_Capability == TCAS_TA) {
                 tcas_equipped = true;
             }
-            if(tcas_equipped){
-                if(Two_of_Three_Reports_Valid){
-                    if(Other_RAC == NO_INTENT){
+            if (tcas_equipped) {
+                if (Two_of_Three_Reports_Valid) {
+                    if (Other_RAC == NO_INTENT) {
                         intent_not_known = true;
                     }
                 }
-                if(intent_not_known){
+                if (intent_not_known) {
                     alt_sep = alt_assign();
                 }
-            }
-            else{
+            } else {
                 alt_sep = alt_assign();
             }
         }
@@ -248,64 +230,49 @@ public class tcas2 {
         return alt_sep;
     }
 
-    public static void mainProcess(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10, int a11, int a12,
-                                   int a21, int a22, int a23, int a24, int a25, int a26, int a27, int a28, int a29, int a30, int a31, int a32) {
+    public static void mainProcess(int Cur_Vertical_Sep, int High_Confidence_flag, int Two_of_Three_Reports_Valid_flag,
+                                   int Own_Tracked_Alt, int Own_Tracked_Alt_Rate, int Other_Tracked_Alt,
+                                   int Alt_Layer_Value, int Up_Separation, int Down_Separation, int Other_RAC, int Other_Capability, int Climb_Inhibit) {
         initialize();
-        Cur_Vertical_Sep = a1;
-        if (a2 == 0) {
-            High_Confidence = false;
+        DiscoveryTCAS.Cur_Vertical_Sep = Cur_Vertical_Sep;
+        if (High_Confidence_flag == 0) {
+            DiscoveryTCAS.High_Confidence = false;
+        } else {
+            DiscoveryTCAS.High_Confidence = true;
         }
-        else {
-            High_Confidence = true;
-        }
-        if (a3 == 0) {
-            Two_of_Three_Reports_Valid = false;
-        }
-        else {
-            Two_of_Three_Reports_Valid = true;
+        if (Two_of_Three_Reports_Valid_flag == 0) {
+            DiscoveryTCAS.Two_of_Three_Reports_Valid = false;
+        } else {
+            DiscoveryTCAS.Two_of_Three_Reports_Valid = true;
         }
 
-        Own_Tracked_Alt = a4;
-        Own_Tracked_Alt_Rate = a5;
-        Other_Tracked_Alt = a6;
-        Alt_Layer_Value = a7;
-        Up_Separation = a8;
-        Down_Separation = a9;
-        Other_RAC = a10;
-        Other_Capability = a11;
-        Climb_Inhibit = a12;
+        DiscoveryTCAS.Own_Tracked_Alt = Own_Tracked_Alt;
+        DiscoveryTCAS.Own_Tracked_Alt_Rate = Own_Tracked_Alt_Rate;
+        DiscoveryTCAS.Other_Tracked_Alt = Other_Tracked_Alt;
+        DiscoveryTCAS.Alt_Layer_Value = Alt_Layer_Value;
+        DiscoveryTCAS.Up_Separation = Up_Separation;
+        DiscoveryTCAS.Down_Separation = Down_Separation;
+        DiscoveryTCAS.Other_RAC = Other_RAC;
+        DiscoveryTCAS.Other_Capability = Other_Capability;
+        DiscoveryTCAS.Climb_Inhibit = Climb_Inhibit;
 
-        alt_sep_test();
+        result_alt_sep_test = alt_sep_test();
+    }
 
-        Cur_Vertical_Sep = a21;
-        if (a22 == 0) {
-            High_Confidence = false;
+    public static void discoveryMainProcess(int Cur_Vertical_Sep, int High_Confidence_flag,
+                                            int Two_of_Three_Reports_Valid_flag,
+                                            int Own_Tracked_Alt, int Own_Tracked_Alt_Rate, int Other_Tracked_Alt,
+                                            int Alt_Layer_Value, int Up_Separation, int Down_Separation, int Other_RAC,
+                                            int Other_Capability, int Climb_Inhibit, boolean symVar) {
+        if (symVar) {
+            mainProcess(Cur_Vertical_Sep, High_Confidence_flag, Two_of_Three_Reports_Valid_flag, Own_Tracked_Alt,
+                    Own_Tracked_Alt_Rate, Other_Tracked_Alt, Alt_Layer_Value, Up_Separation, Down_Separation,
+                    Other_RAC, Other_Capability, Climb_Inhibit);
         }
-        else {
-            High_Confidence = true;
-        }
-        if (a23 == 0) {
-            Two_of_Three_Reports_Valid = false;
-        }
-        else {
-            Two_of_Three_Reports_Valid = true;
-        }
-
-        Own_Tracked_Alt = a24;
-        Own_Tracked_Alt_Rate = a25;
-        Other_Tracked_Alt = a26;
-        Alt_Layer_Value = a27;
-        Up_Separation = a28;
-        Down_Separation = a29;
-        Other_RAC = a30;
-        Other_Capability = a31;
-        Climb_Inhibit = a32;
-
-        alt_sep_test();
     }
 
     public static void main(String[] argv) {
-        mainProcess(601, -1, 0, -1, 0, 0, 0, 301, 400, 0, 0, 1,
-                601, -1, 0, -1, 0, 0, 0, 301, 400, 0, 0, 1);
+
+        discoveryMainProcess(601, -1, 0, -1, 0, 0, 0, 301, 400, 0, 0, 1, true);
     }
 }
